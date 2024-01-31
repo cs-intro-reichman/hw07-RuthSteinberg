3e38f3ebd2360672a81c6199a711baf22deb0f04
RuthSteinberg <ruth.steinberg@post.runi.ac.il>
2024-01-31 20:59:13 +0200

    upload

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -14,27 +14,46 @@ public class HashTagTokenizer {
 
 		In in = new In(fileName);
 
-		// Your code here
-
+		for(int i=0; i<3000; i++)
+		{
+			String word = (String)in.readLine();
+			dictionary[i]=word;
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		boolean answer=false;
+		for(int i=0; i<3000; i++)
+		{
+			if(dictionary[i].equals(word))
+			{
+				answer=true;
+			}
+		}
+		return answer;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
 
 		// Base case: do nothing (return) if hashtag is an empty string.
+		hashtag=hashtag.toLowerCase();
         if (hashtag.isEmpty()) {
             return;
         }
- 
         int N = hashtag.length();
-
         for (int i = 1; i <= N; i++) {
-		
-        }
-    }
+			String newStr=hashtag.substring(0, i);
 
+		if((existInDictionary(newStr, dictionary))==true)
+		{
+			System.out.println(newStr);
+			hashtag=hashtag.substring(i);
+			breakHashTag(hashtag, dictionary);
+			break;
+		}
+		}
+    }
 }
+
+
